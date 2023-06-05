01923ec677eb6d47266ad625118512441ef3e253
HamadaAssi <hamada98assi@gmail.com>
2023-06-05 20:49:29 +0300

    hamada ok ...

--- app/src/main/java/edu/bzu/hotelproject/LoginActivity.java
+++ app/src/main/java/edu/bzu/hotelproject/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HotelProfile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     public void toSignup(View view) {
