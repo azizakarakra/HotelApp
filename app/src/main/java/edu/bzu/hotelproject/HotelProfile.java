125a99003a5798d41499597f5434f6ffff57b1ea
HamadaAssi <hamada98assi@gmail.com>
2023-06-09 20:22:14 +0300

    hotel activity in 9/6 ...

--- app/src/main/java/edu/bzu/hotelproject/HotelProfile.java
+++ app/src/main/java/edu/bzu/hotelproject/HotelProfile.java
@@ -2,13 +2,39 @@ package edu.bzu.hotelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HotelProfile extends AppCompatActivity {
+
+    ImageView menu, appLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel_profile);
+
+        menu = (ImageView) findViewById(R.id.menu);
+        appLogo = (ImageView) findViewById(R.id.appLogo);
+
+        appLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HotelProfile.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HotelProfile.this, User_Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
