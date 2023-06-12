63ed9591dcd7b3524b1ed5ab79313cc4ec783137
HamadaAssi <hamada98assi@gmail.com>
2023-06-12 23:23:18 +0300

    all booking process ...

---
+++ app/src/main/java/edu/bzu/hotelproject/PaymentActivity.java
@@ -0,0 +1,98 @@
+package edu.bzu.hotelproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private Button next, cancel, checkDatePickerButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        next = (Button) findViewById(R.id.next);
+        cancel = (Button) findViewById(R.id.cancel);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PaymentActivity.this, BookingOverviewActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PaymentActivity.this, FillInfoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        initDatePicker();
+        checkDatePickerButton = findViewById(R.id.checkDatePickerButton);
+        checkDatePickerButton.setText(getTodaysDate());
+
+        checkDatePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePicker(view);
+            }
+        });
+    }
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month += 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener startDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month += 1;
+                String newDate = makeDateString(day, month, year);
+                checkDatePickerButton.setText(newDate);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this, style, startDateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        String[] monthsNames = new String[]{"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+        if (month >= 1 && month <= 12) {
+            return monthsNames[month - 1];
+        } else {
+            return "JAN"; // default should never happen
+        }
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
+    }
+}
