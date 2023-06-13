ddead5b5957807a2bee927592e627ecb1b818c02
HamadaAssi <hamada98assi@gmail.com>
2023-06-13 20:03:00 +0300

    hamada assi ...

--- app/src/main/java/edu/bzu/hotelproject/HotelProfile.java
+++ app/src/main/java/edu/bzu/hotelproject/HotelProfile.java
@@ -1,14 +1,31 @@
 package edu.bzu.hotelproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HotelProfile extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HotelProfile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     ImageView menu, appLogo;
     Button reservation;
@@ -18,26 +35,46 @@ public class HotelProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel_profile);
 
-        menu = (ImageView) findViewById(R.id.menu);
-        appLogo = (ImageView) findViewById(R.id.appLogo);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set custom title text
+        getSupportActionBar().setTitle("Home");
+        // Change the color of the Toolbar title
+//        toolbar.setTitleTextColor(getResources().getColor(R.color.orange_color_2));
+//        // Change the default icon color
+//        Drawable navigationIcon = toolbar.getNavigationIcon();
+//        toolbar.setBackgroundColor(getResources().getColor(R.color.white));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        menu = (ImageView) findViewById(R.id.menu);
+//        appLogo = (ImageView) findViewById(R.id.appLogo);
         reservation = (Button)findViewById(R.id.reservation);
 
-        appLogo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HotelProfile.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HotelProfile.this, User_Profile.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        appLogo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(HotelProfile.this, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+//        menu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(HotelProfile.this, User_Profile.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
 
         reservation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,5 +85,42 @@ public class HotelProfile extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Toast.makeText(this, "home!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this, "settings!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(this, "share!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_about:
+                Toast.makeText(this, "about!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
