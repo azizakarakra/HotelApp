05f717952ef8ecb205071355b88c367733e7f1ad
azizakarakra <azizakarakra@gmail.com>
2023-06-21 00:47:29 +0300

    azizaEdit

--- app/src/main/java/edu/bzu/hotelproject/LoginActivity.java
+++ app/src/main/java/edu/bzu/hotelproject/LoginActivity.java
@@ -3,16 +3,33 @@ package edu.bzu.hotelproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String NAME = "NAME";
+    public static final String PASS = "PASS";
+    public static final String FLAG = "FLAG";
+    private boolean flag = false;
+    private EditText edtName;
+    private EditText edtPassword;
+    private CheckBox chk;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupViews();
+        setupSharedPrefs();
+        checkPrefs();
 
         Button login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +46,44 @@ public class LoginActivity extends AppCompatActivity {
     public void toSignup(View view) {
         Intent intent = new Intent(this,SignupActivity.class);
         startActivity(intent);
+    }
+
+    private void checkPrefs() {
+        flag = prefs.getBoolean(FLAG, false);
+
+        if(flag){
+            String name = prefs.getString(NAME, "");
+            String password = prefs.getString(PASS, "");
+            edtName.setText(name);
+            edtPassword.setText(password);
+            chk.setChecked(true);
+        }
+    }
+
+    private void setupSharedPrefs() {
+        prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        editor = prefs.edit();
+    }
+
+    private void setupViews() {
+        edtName = findViewById(R.id.editTextTextPersonName);
+        edtPassword = findViewById(R.id.editTextPass);
+        chk = findViewById(R.id.checkBox);
+    }
+
+    public void btnLoginOnClick(View view) {
+        String name = edtName.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        if(chk.isChecked()){
+            if(!flag) {
+                editor.putString(NAME, name);
+                editor.putString(PASS, password);
+                editor.putBoolean(FLAG, true);
+                editor.commit();
+            }
+
+        }
+
     }
 }
--- app/src/main/java/edu/bzu/hotelproject/SignupActivity.java
+++ app/src/main/java/edu/bzu/hotelproject/SignupActivity.java
@@ -2,7 +2,9 @@ package edu.bzu.hotelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void toLogin(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
